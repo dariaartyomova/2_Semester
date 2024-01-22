d2dfd21fc32a7654873cd98169a4b8ff6d3fb40c
dariaartyomova <144615378+dariaartyomova@users.noreply.github.com>
2024-01-22 17:43:09 +0200

    Add files via upload

---
+++ laba_2_1/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <fstream>
+#include "func.hpp"
+
+int main()
+{
+    std::ofstream out("input.txt", std::ios::app);
+	std::cout << dateyear() << std::endl;
+	out << " " << dateyear()+1900 << "/" << datemon() + 1 << "/" << dateday() << "/" << datehour() << "/" << datemin() << "/" << datesec();
+
+	return 0;
+}
