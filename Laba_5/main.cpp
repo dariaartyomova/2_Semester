91ec5e903d776eff0c79178c18a284f62a5fbcec
dariaartyomova <144615378+dariaartyomova@users.noreply.github.com>
2024-04-06 03:56:44 +0200

    Add files via upload

---
+++ Laba_5/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <SFML/Graphics.hpp>
+#include "circle.hpp"
+#include "game.hpp"
+
+int main()
+{
+    mt::Game game(1100, 800, "Game");
+    game.Setup(5);
+    game.Life();
+
+    return 0;
+}
